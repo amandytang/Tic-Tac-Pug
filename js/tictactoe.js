d08726643f5105d355ebb1d19f135bdabc51cdf8
Mandy Tang <mandytang@mandys-mbp.home>
2018-03-11 13:04:31 +1100

    finished game logic apart from success/draw message

--- js/tictactoe.js
+++ js/tictactoe.js
@@ -4,30 +4,7 @@ let outcome = ["_", "_", "_",
                "_", "_", "_",
                "_", "_", "_", ]; // Empty array to store moves later
 
-
-// We want a function that loops through the outcome array, telling us where all the X's are
-let checkForXWin = function () {
-  let indices = [];
-  let index;
-
-  for (let i = 0; i < outcome.length; i++) {
-    outcome.filter (function (outcome, index) { // This retrieves the index of every occurrence of "X"
-     if (outcome == "X") {
-       indices.push(index);
-     }
-  });
-//  console.log(indices.toString());
-
-//  if (indices.toString() === one of the indices of winningcombo.toString) we have a win
-//we need another loop, just not sure if it goes here 
-
-    return;
-
-  }
-}
-
-
-
+let gameWon = false;
 
 let turns = 0;
 
@@ -42,8 +19,54 @@ let winningCombo = [
   [2,5,8],
 ]
 
- //run something that checks for a win after the third turn (last two if statements)
- // how would you compare outcome array with winningCombo array? i.e. if outcome[]
+// Loop through the outcome array, telling us where all the X's are
+let checkForXWin = function () {
+  let indicesX = [];
+  let index;
+
+  for (let i = 0; i < outcome.length; i++) {
+    outcome.filter (function (outcome, index) { // This retrieves the index of every occurrence of "X"
+     if (outcome == "X") {
+       indicesX.push(index);
+     }
+    });
+
+  for (let j = 0; j < winningCombo.length; j++) {
+  // We can't compare indicesX and outcome arrays directly because it's possible not to win in 3 moves e.g. [0,4,6,8] is never going to equal [0,4,8]. Instead, look for each element of a winningCombo in the indicesX array. If every element is present, X has won.
+    if (indicesX.indexOf(winningCombo[j][0]) > -1
+      && indicesX.indexOf(winningCombo[j][1]) > -1
+      && indicesX.indexOf(winningCombo[j][2]) > -1) {
+        gameWon = true;
+        return;
+        }
+    }
+  }
+return;
+}
+
+
+let checkForOWin = function () {
+  let indicesO = [];
+
+  for (let i = 0; i < outcome.length; i++) {
+    outcome.filter (function (outcome, index) {
+     if (outcome == "O") {
+       indicesO.push(index);
+     }
+    });
+
+  for (let j = 0; j < winningCombo.length; j++) {
+    if (indicesO.indexOf(winningCombo[j][0]) > -1
+      && indicesO.indexOf(winningCombo[j][1]) > -1
+      && indicesO.indexOf(winningCombo[j][2]) > -1) {
+        gameWon = true;
+        return;
+        }
+      }
+    }
+return;
+}
+
 
 $('.square').on('click', function () {
 
@@ -51,19 +74,28 @@ $('.square').on('click', function () {
     return;
   }
 
-  if (turns === 9) {
+  if (gameWon) {
     return;
   }
 
-  if (turns === 0) {// If it's the first move of player 1
+  if (turns === 8) { // It's automatically a draw if it's turn 8 and the game hasn't been won yet
+    console.log('turn 8');
+  }
+
+  if ($(this).find('img').attr('src') === './css/images/donutpug.png') { // Prevent changing existing move
+    return;
+  }
+
+  if ($(this).find('img').attr('src') === './css/images/unipug.png') {
+    return;
+  }
+
+  if (turns === 0) {
     outcome[+this.id] = "X";
-    turns = turns + 1;   // number of turns needs to count up each time
-    // console.log(this.id);
+    turns = turns + 1;
 
     $(this).find('img').css('visibility', 'visible');
     $(this).find('img').attr('src', './css/images/donutpug.png');
-    console.log(turns);
-    console.log(outcome);
     return turns;
   }
 
@@ -73,16 +105,12 @@ $('.square').on('click', function () {
 
     $(this).find('img').css('visibility', 'visible');
     $(this).find('img').attr('src', './css/images/unipug.png');
-    console.log(turns);
-    console.log(outcome);
     return turns;
   }
 
   if (turns !== 0 && turns % 2 === 0) {
     outcome[+this.id] = "X";
     turns = turns + 1;
-    console.log(turns);
-    console.log(outcome);
 
     $(this).find('img').css('visibility', 'visible');
     $(this).find('img').attr('src', './css/images/donutpug.png');
@@ -95,13 +123,11 @@ $('.square').on('click', function () {
   if (turns !== 0 && turns % 2 !== 0) {
     outcome[+this.id] = "O";
     turns = turns + 1;
-    console.log(turns);
-    console.log(outcome);
 
     $(this).find('img').css('visibility', 'visible');
     $(this).find('img').attr('src', './css/images/unipug.png');
 
-//    checkForOWin();
+    checkForOWin();
 
     return turns;
     }
@@ -110,16 +136,14 @@ $('.square').on('click', function () {
 
 
 
-
 $('h1').funText((40, ['#7dbcdb', '#edd07b', '#ba5b62']));
 
 }); // DOM readiness
 
 
-// TODO: prevent player from clicking a square that's already taken (check if img already has a pug src)
 // TODO: Make hover for h1 tag - from toe to pug
 // TODO: Marquee success message or pug gif
 // TODO: sound of barking
 // TODO: add alt text e.g. alt="Pug in a donut costume"
 
-//6h as of 11.30 saturday
+//7.5h as of 1pm on sunday
